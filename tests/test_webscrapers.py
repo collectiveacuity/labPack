1c4a74922df80e04b1c48053e5a6cba8670145dd
rcj1492 <richard@collectiveacuity.com>
2016-02-14 20:32:23 -0800

    reorganized project structure for pyModules

--- tests/test_webscrapers.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'rcj1492'
-__created__ = '2016.02'
-
-from cred.credentialsMorphIO import morphIOCredentials
-from labpack.webscrapers import labMorph
-
-labMorph(morphIOCredentials).unitTests()
-
