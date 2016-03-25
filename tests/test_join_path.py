7e2edb5a0ab5840080c780780b6b214ee09087ef
rcj1492 <richard@collectiveacuity.com>
2016-03-24 18:18:57 -0700

    refactoring tests to roll into dev structure;

--- tests/test_join_path.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'rcj1492'
-__created__ = '2016.02'
-
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
--- tests/test_shorteners.py
+++
@@ -1,7 +0,0 @@
-__author__ = 'rcj1492'
-__created__ = '2016.02'
-
-from cred.credentialsBitly import bitlyToken
-from dev.shorteners import bitlyAPI
-
-assert bitlyAPI('https://twitter.com/collectiveacuiT', bitlyToken)
