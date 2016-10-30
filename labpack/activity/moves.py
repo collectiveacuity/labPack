d4fd3516c7bf886b1caabfa403386e0f48d932bb
rcj1492 <richard@collectiveacuity.com>
2016-10-30 13:55:03 -0700

    added access token get and renew methods to movesOAuth class; untested due to third-party resource issue;

--- labpack/activity/moves.py
+++ labpack/activity/moves.py
@@ -16,18 +16,26 @@ class movesOAuth(object):
                 'web': 'https://api.moves-app.com/oauth/v1',
                 'mobile': 'moves://app'
             },
+            'token_details': {
+                'access_token': '',
+                'token_type': '',
+                'expires_in': 0,
+                'refresh_token': '',
+                'user_id': 0
+            },
             'client_id': '',
             'client_secret': '',
             'device_type': '',
-            'redirect_url': '',
+            'redirect_uri': '',
             'service_scope': [ 'location' ],
-            'state_value': ''
+            'state_value': '',
+            'access_code': ''
         },
         'components': {
             '.device_type': {
                 'discrete_values': [ 'web', 'mobile' ]
             },
-            '.redirect_url': {
+            '.redirect_uri': {
                 'must_contain': [ '^https://' ]
             },
             '.service_scope': {
@@ -50,14 +58,14 @@ class movesOAuth(object):
         self.clientID = self.fields.validate(client_id, '.client_id')
         self.clientSecret = self.fields.validate(client_secret, '.client_secret')
 
-    def generate_url(self, device_type, redirect_url, service_scope, state_value=''):
+    def generate_url(self, device_type, redirect_uri, service_scope, state_value=''):
 
         title = '%s.generate_url' % self.__class__.__name__
 
     # validate inputs
         input_args = {
             'device_type': device_type,
-            'redirect_url': redirect_url,
+            'redirect_uri': redirect_uri,
             'service_scope': service_scope,
             'state_value': state_value
         }
@@ -72,7 +80,7 @@ class movesOAuth(object):
     # construct query parameters
         query_params = {
             'client_id': self.clientID,
-            'redirect_url': redirect_url,
+            'redirect_uri': redirect_uri,
             'scope': ''
         }
         for item in service_scope:
@@ -90,12 +98,77 @@ class movesOAuth(object):
 
         return url_string
 
-    def get_token(self, access_code):
-        token_details = {}
+    def get_token(self, access_code, redirect_uri):
+
+        title = '%s.get_token' % self.__class__.__name__
+
+    # validate inputs
+        input_args = {
+            'access_code': access_code,
+            'redirect_uri': redirect_uri
+        }
+        for key, value in input_args.items():
+            if value:
+                object_title = '%s(%s=%s)' % (title, key, value)
+                self.fields.validate(value, '.%s' % key, object_title)
+
+    # construct url string
+        url_string = '%s/access_token' % self.fields.schema['oauth_endpoint']['web']
+
+    # construct request params
+        request_params = {
+            'grant_type': 'authorization_code',
+            'code': access_code,
+            'client_id': self.clientID,
+            'client_secret': self.clientSecret,
+            'redirect_uri': redirect_uri
+        }
+
+    # send request
+        import requests
+        response = requests.post(url_string, params=request_params)
+        if response.status_code == 200:
+            token_details = response.json()
+        else:
+            raise Exception('%s returned status code %s' % (title, response.status_code))
+
         return token_details
 
-    def renew_token(self, access_token):
-        token_details = {}
+    def renew_token(self, refresh_token, service_scope=None):
+
+        title = '%s.renew_token' % self.__class__.__name__
+
+    # validate inputs
+        object_title = '%s(%s=%s)' % (title, 'refresh_token', refresh_token)
+        self.fields.validate(refresh_token, '.token_details.refresh_token', object_title)
+        if service_scope:
+            object_title = '%s(%s=%s)' % (title, 'service_scope', service_scope)
+            self.fields.validate(service_scope, '.service_scope', object_title)
+
+    # construct url string
+        url_string = '%s/access_token' % self.fields.schema['oauth_endpoint']['web']
+
+    # construct request params
+        request_params = {
+            'grant_type': 'refresh_token',
+            'refresh_token': refresh_token,
+            'client_id': self.clientID,
+            'client_secret': self.clientSecret
+        }
+        if service_scope:
+            for item in service_scope:
+                if request_params['scope']:
+                    request_params['scope'] += ' '
+                request_params['scope'] += item
+
+    # send request
+        import requests
+        response = requests.post(url_string, params=request_params)
+        if response.status_code == 200:
+            token_details = response.json()
+        else:
+            raise Exception('%s returned status code %s' % (title, response.status_code))
+
         return token_details
 
     def check_token(self):
@@ -120,8 +193,8 @@ if __name__ == '__main__':
     moves_cred = load_settings('../../../cred/moves.yaml')
     client_id = moves_cred['oauth_client_id']
     client_secret = moves_cred['oauth_client_secret']
-    redirect_url = moves_cred['oauth_redirect_url']
+    redirect_uri = moves_cred['oauth_redirect_url']
     moves_oauth = movesOAuth(client_id, client_secret)
-    auth_url = moves_oauth.generate_url('web', redirect_url, ['location', 'activity'])
-    assert auth_url.find('code') > 1
+    auth_url = moves_oauth.generate_url('web', redirect_uri, ['location', 'activity'])
+    assert auth_url.find('code') > 0
 
