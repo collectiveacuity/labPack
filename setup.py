fe6af80673a2e18537ab5353967d18715b5b004e
rcj1492 <richard@collectiveacuity.com>
2017-06-08 21:23:28 -0400

    starting migration to mercurial; added mkdocs framework;

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
-import re
 from setuptools import setup, find_packages
+from pocketlab.methods.config import inject_init
 
-'''
+init_path = 'pocketlab/__init__.py'
+readme_path = 'README.rst'
+setup_kwargs = {
+    'include_package_data': True, # Checks MANIFEST.in for explicit rules
+    'packages': find_packages(),
+    'install_requires': [
+        'pytz>=2015.7',
+        'tzlocal>=1.2',
+        'python-dateutil>=2.4.2',
+        'pyyaml>=3.12',
+        'jsonmodel',
+        'requests',
+        'PyJWT>=1.4.2'
+    ],
+    'classifiers': [
+        # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5'
+    ]
+}
+setup_kwargs = inject_init(init_path, readme_path, setup_kwargs)
+setup(**setup_kwargs)
+
+
+''' DOCUMENTATION
 References:
+https://docs.python.org/3.6/distutils/setupscript.html
+
 https://python-packaging-user-guide.readthedocs.org/en/latest/
-https://docs.python.org/3.5/distributing/index.html#distributing-index
+https://docs.python.org/3.5/distutils/index.html
 https://github.com/jgehrcke/python-cmdline-bootstrap
 http://www.pyinstaller.org/
 
@@ -14,16 +45,20 @@ pip install twine
 
 Build Distributions:
 python setup.py sdist --format=gztar,zip
-pip wheel --no-index --no-deps --wheel-dir dist dist/labpack-0.9.tar.gz
+pip wheel --no-index --no-deps --wheel-dir dist dist/pocketlab-0.1.tar.gz
 
-Upload Distributions to PyPi:
-python setup.py register
+Upload (or Register) Distributions to PyPi:
 twine upload dist/[module-version]*
+
+Upload Documentation to Github:
+mkdocs gh-deploy
+.gitconfig [credential] helper = wincred
 
 Installation:
 pip install [module]
 python setup.py develop  # for local on-the-fly file updates
 python setup.py install  # when possessing distribution files
+pip install dist/pocketlab-0.1-py3-none-any.whl # when possessing wheel file
 
 Uninstall:
 pip uninstall [module]
@@ -33,78 +68,27 @@ python setup.py develop --uninstall # for removing symbolic link
 CLI Installation:
 command = 'name of command'
 module = 'name of module'
-    entry_points = {
-        "console_scripts": ['%s = %s.cli:cli' % (command, module)]
-    },
+entry_points = {
+    "console_scripts": ['%s = %s.cli:cli' % (command, module)]
+},
 
 System Installation:
 # http://www.pyinstaller.org/
 
+Mercurial Dev Setup:
+.hgignore (add dist/, *.egg-info/, '.git/')
+hgrc [paths] default = ssh://hg@bitbucket.org/collectiveacuity/pocketlab
+
 Git Public Setup:
-https://stackoverflow.com/questions/37422221/git-remove-a-file-from-a-branch-keep-it-in-the-master/37422311
-git remote add github https://github.com/...
-git branch public
-git checkout public
-git rm --cached --ignore-unmatch -r dev/*
-git rm --cached --ignore-unmatch -r tests_dev/*
-git rm --cached --ignore-unmatch -r notes/*
-git commit -m 'removed dev files before public push'
-git push github public
-git checkout -f master
+.gitignore (add dist/, *.egg-info/, dev/, tests_dev/, docs/, docs_dev/, .hg/, .hgignore)
+git init
+git remote add origin https://github.com/collectiveacuity/pocketLab.git
 
 Git Public Updates:
-https://stackoverflow.com/questions/14369378/how-to-make-git-ignore-a-directory-while-merging
-git checkout public
-git merge --no-commit --no-ff master
-git reset -- dev/*
-git commit -m 'master into public'
-git push github public
-git checkout -f master
+git add -A
+git commit -m 'updates'
+git push origin master
 
-Git Remove History:
-https://stackoverflow.com/questions/2004024/how-to-permanently-delete-a-file-stored-in-git
-[Run as admin and pause syncing]
+Git Remove History: [Run as admin and pause syncing]
 git filter-branch --force --index-filter 'git rm -rf --cached --ignore-unmatch dev/*' --prune-empty --tag-name-filter cat -- --all
 '''
-
-config_file = open('labpack/__init__.py').read()
-version = re.search("^__version__\s*=\s*'(.*)'", config_file, re.M).group(1)
-# command = re.search("^__command__\s*=\s*'(.*)'", config_file, re.M).group(1)
-license_terms = re.search("^__license__\s*=\s*'(.*)'", config_file, re.M).group(1)
-module = re.search("^__module__\s*=\s*'(.*)'", config_file, re.M).group(1)
-author = re.search("^__author__\s*=\s*'(.*)'", config_file, re.M).group(1)
-email = re.search("^__email__\s*=\s*'(.*)'", config_file, re.M).group(1)
-url = re.search("^__url__\s*=\s*'(.*)'", config_file, re.M).group(1)
-# author_list = re.search("^__authors__\s*=\s*'(.*)'", config_file, re.M).group(1)
-
-setup(
-    name=module,
-    version=version,
-    author=author,
-    author_email=email,
-    maintainer_email=email,
-    url=url,
-    include_package_data=True,  # Checks MANIFEST.in for explicit rules
-    packages=find_packages(),  # exclude=['cred','data','keys','dev','docs','tests','tests_dev','models','notes','tinkering'] Needed for bdist
-    license=license_terms,
-    description="A Collection of Methods for Data Collection & Processing",
-    long_description=open('README.rst').read(),
-    install_requires=[
-        'pytz>=2015.7',
-        'tzlocal>=1.2',
-        'python-dateutil>=2.4.2',
-        'pyyaml>=3.12',
-        'jsonmodel',
-        'requests',
-        'PyJWT>=1.4.2'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5'
-    ]
-)
