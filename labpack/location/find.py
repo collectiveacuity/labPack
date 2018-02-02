c1bd54915a8f0743c07e514909a426d3f5bdf17e
/home/rcj1492 <richard@collectiveacuity.com>
2018-02-02 03:30:07 +0000

    added client class to connect to FIND server for indoor positioning;

---
+++ labpack/location/find.py
@@ -0,0 +1,398 @@
+__author__ = 'rcj1492'
+__created__ = '2018.02'
+__license__ = 'MIT'
+
+class findClient(object):
+
+    def __init__(self, group_name, server_url='ml.internalpositioning.com', password=''):
+
+        '''
+            a method to initialize a findClient class object
+
+        :param group_name: string with name of group 
+        :param server_url: string with url for FIND server
+        :param password: [optional] string with password to mosquitto server
+
+        # https://www.internalpositioning.com/api
+        '''
+
+    # construct class properties
+        self.server_url = server_url
+        self.endpoint = 'https://%s' % server_url
+        self.endpoint_public = 'http://%s' % server_url
+        self.group_name = group_name
+        self.password = password
+        self.positions = {}
+        self.locations = {}
+
+    # add regex patterns
+        import re
+        self.user_pattern = re.compile('location/(.*)$')
+
+    def get_password(self):
+
+        '''
+            a method to retrieve the password for the group mosquitto server
+
+        :return: string with group mosquitto server password
+
+        NOTE:   result is added to self.password property 
+        '''
+
+        import requests
+        url = '%s/mqtt' % self.endpoint
+        params = {
+            'group': self.group_name
+        }
+        response = requests.put(url, params=params)
+        response_details = response.json()
+        self.password = response_details['password']
+
+        return self.password
+
+    def get_locations(self):
+
+        '''
+            a method to retrieve all the locations tracked by the model
+            
+        :return: dictionary with location id keys
+        
+        NOTE:   results are added to self.locations property
+        
+        {
+            'location.id': {
+                '
+            }
+        }
+        '''
+        
+        import requests
+        url = self.endpoint + '/locations'
+        params = {
+            'group': self.group_name
+        }
+        response = requests.get(url, params=params)
+        response_details = response.json()
+        if 'locations' in response_details.keys():
+            self.locations = response_details['locations']
+            
+        return self.locations
+
+    def update_model(self):
+        
+        '''
+             a method to update model with latest training data
+             
+        :return: True
+        '''
+        
+        import requests
+        url = self.endpoint_public + '/calculate'
+        params = {
+            'group': self.group_name
+        }
+        response = requests.get(url, params=params)
+        response_details = response.json()
+        
+        return response_details['success']
+        
+    def get_position(self, user_id, track=False, confidence=False):
+
+        '''
+            a method to retrieve the latest position of a user
+
+        :param user_id: string with id of user
+        :param track: [optional] boolean to add user to self.positions
+        :param confidence: [optional] boolean to include the data model confidence scores
+        :return: dictionaries with position details
+
+        NOTE:   if user does not exist, then location and time are null values
+
+        { 
+            'time': 0.0, 
+            'location': 'location.id', 
+            'id': 'user_id', 
+            bayes: {}, # if confidence = True 
+            svm: None, # if confidence = True 
+            rf: {} # if confidence = True 
+        }
+        '''
+        
+    # construct empty response
+        position_details = {
+            'location': '',
+            'time': 0.0,
+            'id': user_id
+        }
+
+    # construct empty position history
+        position_history = []
+        
+    # compose request
+        import requests
+        url = self.endpoint + '/location'
+        params = {
+            'group': self.group_name,
+            'user': user_id,
+            'n': 1
+        }
+        response = requests.get(url, params=params)
+
+    # ingest response
+        response_details = response.json()
+        from labpack.records.time import labDT
+        for key in response_details['users'].keys():
+            for entry in response_details['users'][key]:
+                if 'time' in entry.keys() and 'location' in entry.keys():
+                    time_string = entry['time'] 
+                    time_string = time_string.replace(' +0000 UTC', 'Z')
+                    time_string = time_string.replace(' ', 'T')
+                    time_dt = labDT.fromISO(time_string).epoch()
+                    if confidence:
+                        for key, value in entry.items():
+                            position_details[key] = value
+                    position_details['time'] = time_dt
+                    position_details['location'] = entry['location']
+                    break
+
+        if track:
+            stored_position = {
+                'location': position_details['location'],
+                'time': position_details['time']
+            }
+            self.positions[user_id] = stored_position
+
+        return position_details
+
+    def get_positions(self, user_id, history=1, confidence=False):
+        
+        '''
+            a method to retrieve the position history of a user
+            
+        :param user_id: string with id of user
+        :param history: [optional] integer with length of previous positions to retrieve
+        :param confidence: [optional] boolean to include the data model confidence scores
+        :return: list of dictionaries with position details
+        
+        NOTE:   if user does not exist, then list returns empty
+        
+        [{ 
+            'time': 0.0, 
+            'location': 'location.id', 
+            'id': 'user_id', 
+            bayes: {}, # if confidence = True 
+            svm: None, # if confidence = True 
+            rf: {} # if confidence = True 
+        }]
+        '''
+        
+    # construct empty position history
+        position_history = []
+        
+    # compose request
+        import requests
+        url = self.endpoint + '/location'
+        params = {
+            'group': self.group_name,
+            'user': user_id,
+            'n': history
+        }
+        response = requests.get(url, params=params)
+
+    # ingest response
+        response_details = response.json()
+        from labpack.records.time import labDT
+        for key in response_details['users'].keys():
+            for entry in response_details['users'][key]:
+                position_details = {}
+                if 'time' in entry.keys() and 'location' in entry.keys():
+                    time_string = entry['time'] 
+                    time_string = time_string.replace(' +0000 UTC', 'Z')
+                    time_string = time_string.replace(' ', 'T')
+                    time_dt = labDT.fromISO(time_string).epoch()
+                    if confidence:
+                        for key, value in entry.items():
+                            position_details[key] = value
+                    position_details['time'] = time_dt
+                    position_details['location'] = entry['location']
+                    position_history.append(position_details)
+
+        return position_history
+
+    def update_positions(self):
+
+        '''
+            a method to update the latest position of all users being tracked 
+            
+        :return: dictionary with user_id keys and location/time dictionary values
+        
+        NOTE:   self.players is updated with position details
+        
+        {
+            'user_id': {
+                'time': 0.0,
+                'location': 'location.id'
+            }
+        }
+        '''
+        
+    # construct user list
+        user_list = []
+        for key in self.positions.keys():
+            user_list.append(key)
+
+    # return empty result
+        if not user_list:
+            return self.positions
+
+    # compose request
+        import requests
+        url = self.endpoint + '/location'
+
+        while user_list:
+
+        # batch requests
+            if len(user_list) > 50:
+                user_batch = user_list[0:50]
+            else:
+                user_batch = user_list
+            params = {
+                'group': self.group_name,
+                'users': ','.join(user_batch),
+                'n': 1
+            }
+            response = requests.get(url, params=params)
+    
+        # ingest response
+            response_details = response.json()
+            from labpack.records.time import labDT
+            for key in response_details['users'].keys():
+                position_details = {}
+                for entry in response_details['users'][key]:
+                    if 'time' in entry.keys() and 'location' in entry.keys():
+                        time_string = entry['time'] 
+                        time_string = time_string.replace(' +0000 UTC', 'Z')
+                        time_string = time_string.replace(' ', 'T')
+                        time_dt = labDT.fromISO(time_string).epoch()
+                        position_details = {
+                            'time': time_dt,
+                            'location': entry['location']
+                        }
+                        break
+                self.positions[key] = position_details
+
+        # slice user list
+            if len(user_list) > 50:
+                user_list = user_list[50:0]
+            else:
+                user_list = []
+
+        return self.positions
+
+    def subscribe(self, callable=None, block=False, port=1883):
+
+        '''
+            a method to establish a mosquitto socket with server to receive position updates
+            
+        :param callable: [optional] callable to process the received positions
+        :param block: [optional] boolean to block the main thread when method is called
+        :param port: [optional] integer with port to connect to
+        :return: None
+        
+        NOTE:   subscribing without a callable will update the self.positions object
+                with any user message
+
+        { 
+            'time': 0.0, 
+            'location': 'location.id', 
+            'id': 'user_id', 
+            bayes: {}, 
+            svm: None, 
+            rf: {} 
+        }
+
+        # https://www.internalpositioning.com/server/
+        '''
+
+        import paho.mqtt.client as mqtt
+
+    # define callback on connection event
+        def on_connect(client, userdata, flags, rc):
+
+            if rc == 5:
+                raise ValueError('MQTT to %s authorization failed.' % self.group_name)
+
+            print("Connected to %s with result code %s" % (self.group_name, rc))
+
+            # Subscribing in on_connect() means that if we lose the connection and
+            # reconnect then subscriptions will be renewed.
+            client.subscribe("%s/location/#" % self.group_name)
+
+    # define callback on a new message event
+        def on_message(client, userdata, msg):
+
+            import json
+            user_search = self.user_pattern.findall(msg.topic)
+            user_id = user_search[0]
+            user_location = json.loads(msg.payload.decode())
+            user_location['time'] = user_location['time'] / 1000000000
+            user_location['id'] = user_id
+            if callable:
+                callable(user_location)
+            else:
+                self.positions[user_id] = { 
+                    'location': user_location['location'], 
+                    'time': user_location['time']
+                }
+
+    # instantiate client and configure methods
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.username_pw_set(self.group_name, self.password)
+        client.connect_async(self.server_url, port, 60)
+
+    # start blocking listener
+        if block:
+            client.loop_forever()
+    # start listener in separate thread
+        else:
+            client.loop_start()
+ 
+if __name__ == '__main__':
+    
+    from labpack.records.settings import load_settings
+    find_cred = load_settings('../../../cred/find.yaml')
+    find_client = findClient(
+        group_name=find_cred['find_mqtt_group'],
+        password=find_cred['find_mqtt_password']
+    )
+
+# test password
+    password = find_client.get_password()
+    assert password == find_cred['find_mqtt_password']
+
+# test locations
+    locations = find_client.get_locations()
+    print(locations)
+
+# test update model
+    assert find_client.update_model()
+    
+# test positions
+    user_id = find_cred['find_mqtt_admin']
+    positions = find_client.get_positions(user_id, history=2, confidence=True)
+    assert len(positions) == 2
+
+# test position
+    position = find_client.get_position(user_id, track=True)
+    del position['id']
+    position_map = { user_id: position }
+    assert position_map == find_client.positions
+
+# test update positions
+    find_client.update_positions()
+    print(find_client.positions)
+
+# test subscribe
+    find_client.subscribe(callable=print, block=True)
